7e0146b0a9c84af3c29dc52ef626c423a5efca11
Aidan Coward <aidan.coward@gmail.com>
2018-06-11 17:38:11 -0400

    project euler, problem 15

--- project_euler/problem_15/src/main.rs
+++ project_euler/problem_15/src/main.rs
@@ -1,39 +1,30 @@
 fn main() {
+    let test_routes = vec![vec![(0, 0)]];
     
-let mut current_routes: Vec<Vec<(u8, u8)>> = vec![vec![(0, 0)]];
-let mut next_routes: Vec<Vec<(u8, u8)>> = Vec::new();
-let mut working_routes: Vec<Option<(u8, u8)>>, Vec<Option<(u8, u8>>;
+    let output_routes = next_steps(test_routes);
 
-while current_routes.len() < 42 {
-    for route in current_routes {           // for all routes so far
-        working_routes = next_steps(route);
-        match working_routes.0 {           
-            Some(x) => next_routes.push.vec![route.push(x)], // if the returned value is a route, push it onto the list of routes
-            None => (),
-        }
-        match working_routes.1 {
-            Some(y) => next_routes.push.vec![route.push(y)],
-            None => (),
-        }
-        working_routes = [[()]];
+    for x in output_routes {
+        println!("{:?}", x);
     }
-    
-    current_routes = next_routes;
-    next_routes = vec![()];
-    }
-
-println!("{}", current_routes.len);
 }
 
+    
 
-// takes last point of vector and returns the acceptable next points with none if either one
-// doesn't exist
+
+
+
+
+
 fn next_steps(route: Vec<(u8, u8)>) -> (Option<u8>, Option<u8>) {
+    let first_route: Vec<(u8, u8)> = route.clone();
+    let second_route: Vec<(u8, u8)> = route.clone();
     let current_point = route.pop();
     let point_1: Option<(u8, u8)>;
     let point_2: Option<(u8, u8)>;
 
 // add next points based on position of current point
+// option type in case there is no next point
+
     if current_point[0] < 40 { 
         point_1 = Some(current_point[0] + 1, current_point[1]);
     }
@@ -46,11 +37,11 @@ fn next_steps(route: Vec<(u8, u8)>) -> (Option<u8>, Option<u8>) {
         else {
             point_2 = None;
     }
+
+    match point_1 {
+        Some(x) => first_route = 
+
     route.push(current_point);
     (point_1, point_2)
 }
-    
-
-
-    
-    
+   
